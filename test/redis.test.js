d0282ea7e862d44ad7774dbe926a6da646e520f0
Tj Holowaychuk <tj@vision-media.ca>
2011-01-01 16:51:18 -0800

    Added listen()

--- test/redis.test.js
+++ test/redis.test.js
@@ -12,18 +12,6 @@ module.exports = {
     nedis.version.should.match(/^\d+\.\d+\.\d+$/);
   },
   
-  'test createServer()': function(){
-    var server = nedis.createServer();
-    server.should.have.property('port', 6379);
-    server.should.have.property('host', '127.0.0.1');
-  },
-  
-  'test createServer(port, host)': function(){
-    var server = nedis.createServer(3000, '1.1.1.1');
-    server.should.have.property('port', 3000);
-    server.should.have.property('host', '1.1.1.1');
-  },
-  
   'test Server inheritance': function(){
     var server = nedis.createServer();
     server.should.be.an.instanceof(net.Server);
