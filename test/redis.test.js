1ae9f40372137fe9eaa97562166397a182a73739
Tj Holowaychuk <tj@vision-media.ca>
2011-01-01 16:35:13 -0800

    Added Server

--- test/redis.test.js
+++ test/redis.test.js
@@ -9,5 +9,17 @@ var nedis = require('nedis')
 module.exports = {
   'test .version': function(assert){
     nedis.version.should.match(/^\d+\.\d+\.\d+$/);
+  },
+  
+  'test createServer()': function(){
+    var server = nedis.createServer();
+    server.should.have.property('port', 6379);
+    server.should.have.property('host', '127.0.0.1');
+  },
+  
+  'test createServer(port, host)': function(){
+    var server = nedis.createServer(3000, '1.1.1.1');
+    server.should.have.property('port', 3000);
+    server.should.have.property('host', '1.1.1.1');
   }
 };
