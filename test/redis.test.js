80cb360ba0fdf40f3adbb06bfb791516a67a8efc
Tj Holowaychuk <tj@vision-media.ca>
2011-01-02 20:19:42 -0800

    Fixed tests

--- test/redis.test.js
+++ test/redis.test.js
@@ -11,14 +11,14 @@ var server = nedis.createServer().listen('/tmp/nedis')
   , client;
 
 module.exports = {
-  setup: function(done){
+  before: function(done){
     server.on('listening', function(){
       client = net.createConnection('/tmp/nedis');
       client.on('connect', done);
     });
   },
   
-  'test .version': function(assert){
+  'test .version': function(){
     nedis.version.should.match(/^\d+\.\d+\.\d+$/);
   },
   
@@ -28,9 +28,15 @@ module.exports = {
   },
   
   'test GET <key>': function(done){
-    client.write('*3\r\nGET\r\n$3\r\nfoo\r\n');
+    client.write('*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n');
     client.on('data', function(chunk){
-      console.log(chunk.toString());
+      chunk.toString().should.equal('$-1\r\n');
+      done();
     });
+  },
+  
+  after: function(){
+    client.destroy();
+    server.close();
   }
 };
