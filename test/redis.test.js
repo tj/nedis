5ac18b88895f01136a915e33756f57b4dad7641e
Tj Holowaychuk <tj@vision-media.ca>
2011-01-02 17:45:42 -0800

    setting up tests

--- test/redis.test.js
+++ test/redis.test.js
@@ -7,6 +7,9 @@ var nedis = require('nedis')
   , should = require('should')
   , net = require('net');
 
+var server = nedis.createServer().listen('/tmp/nedis')
+  , client = net.createConnection('/tmp/nedis');
+
 module.exports = {
   'test .version': function(assert){
     nedis.version.should.match(/^\d+\.\d+\.\d+$/);
@@ -15,5 +18,12 @@ module.exports = {
   'test Server inheritance': function(){
     var server = nedis.createServer();
     server.should.be.an.instanceof(net.Server);
+  },
+  
+  'test GET <key>': function(done){
+    client.write('*3\r\nGET\r\n$3\r\nfoo\r\n');
+    client.on('data', function(chunk){
+      console.log(chunk.toString());
+    });
   }
 };
