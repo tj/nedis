affd799920e7713f15d75b9ea0b87a8153429537
Tj Holowaychuk <tj@vision-media.ca>
2011-01-03 23:01:06 -0800

    Fixed tests

--- test/redis.test.js
+++ test/redis.test.js
@@ -15,6 +15,12 @@ module.exports = {
     server.on('listening', function(){
       client = net.createConnection('/tmp/nedis');
       client.on('connect', done);
+      client.once = function(event, fn){
+        client.on(event, function callback(){
+          client.removeListener(event, callback);
+          fn.apply(this, arguments);
+        });
+      };
     });
   },
   
@@ -29,7 +35,7 @@ module.exports = {
   
   'test GET <key>': function(done){
     client.write('*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n');
-    client.on('data', function(chunk){
+    client.once('data', function(chunk){
       chunk.toString().should.equal('$-1\r\n');
       done();
     });
@@ -37,7 +43,7 @@ module.exports = {
   
   'test SET <key> <val>': function(done){
     client.write('*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n');
-    client.on('data', function(chunk){
+    client.once('data', function(chunk){
       chunk.toString().should.equal('+OK\r\n');
       done();
     });
@@ -45,7 +51,7 @@ module.exports = {
   
   'test GET invalid args': function(done){
     client.write('*3\r\n$3\r\nGET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n');
-    client.on('data', function(chunk){
+    client.once('data', function(chunk){
       chunk.toString().should.equal("-ERR wrong number of arguments for 'get' command\r\n");
       done();
     });
