593e0ccacd020669556206cefc7f7262f7711280
Tj Holowaychuk <tj@vision-media.ca>
2011-01-05 20:34:19 -0800

    Fixed .gitignore

---
+++ test/test.js
@@ -0,0 +1,133 @@
+
+/**
+ * Module dependencies.
+ */
+
+var nedis = require('../')
+  , net = require('net')
+  , fs = require('fs');
+
+/**
+ * Server.
+ */
+
+nedis.createServer().listen(8888);
+
+/**
+ * Client.
+ */
+
+var client = net.createConnection(8888);
+
+/**
+ * Monkey-patch .once()/
+ *
+ * @param {String} event
+ * @param {String} fn
+ */
+
+client.once = function(event, fn){
+  client.on(event, function callback(chunk){
+    client.removeListener(event, callback);
+    fn(chunk);
+  });
+};
+
+/**
+ * Timeout support.
+ */
+
+client.setEncoding('utf8');
+client.setTimeout(1000);
+client.on('timeout', function(){
+  console.log('timed out');
+  process.exit(1);
+});
+
+/**
+ * Test cases.
+ */
+
+var files = fs.readdirSync(__dirname + '/cases').filter(function(file){
+  return !~file.indexOf('.out');
+});
+
+/**
+ * Pending tests.
+ */
+
+var pending = files.length;
+
+/**
+ * Normalize `str`.
+ *
+ * @return {String}
+ */
+
+function normalize(str) {
+  return str.replace(/\n/g, '\r\n');
+}
+
+/**
+ * Expose hidden chars in `str`.
+ *
+ * @param {String} str
+ * @return {String}
+ */
+
+function expose(str) {
+  return str
+    .replace(/\r\n/g, '\\r\\n\n')
+    .replace(/^/gm, '  ');
+}
+
+/**
+ * Run tests.
+ */
+
+client.on('connect', function(){
+  (function next(){
+    file = files.shift();
+    process.stdout.write('\033[90m...\033[0m \033[33m' + file.toUpperCase() + '\033[0m ');
+
+    // Read test case
+    var input
+      , expected
+      , path = __dirname + '/cases/' + file;
+
+    // input
+    fs.readFile(path, 'utf8', function(err, str){
+      input = normalize(str);
+      input && expected && test();
+    });
+    
+    // output
+    fs.readFile(path + '.out', 'utf8', function(err, str){
+      expected = normalize(str);
+      input && expected && test();
+    });
+
+    function test(){
+      var start = new Date;
+      client.write(input);
+      client.once('data', function(reply){
+        // All good
+        if (expected == reply) {
+          console.log('\033[32m%dms\033[0m', new Date - start);
+          --pending || process.exit(0);
+        // Not so good  
+        } else {
+          console.log('\n');
+          console.log('  \033[31m%s:\033[0m ', 'expected');
+          console.log(expose(reply));
+          console.log('  \033[31m%s:\033[0m ', 'reply');
+          console.log(expose(expected));
+          process.exit(1);
+        }
+
+        // TATFT
+        next();
+      });
+    }
+  })();
+});
