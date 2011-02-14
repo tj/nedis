528d7f26b988854e2bd4cbf1755f385721c36ecf
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2011-02-14 08:33:07 -0500

    tests running on node v0.4.0
    they still don't pass but at least they run!
    
    closes #3

--- test/test.js
+++ test/test.js
@@ -5,134 +5,138 @@
 
 var nedis = require('../')
   , net = require('net')
-  , fs = require('fs');
+  , fs = require('fs')
+  , port = 8888;
 
 /**
  * Server.
  */
 
-nedis.createServer().listen(8888);
+var server = nedis.createServer();
+server.on('listening', function startTest () {
 
-/**
- * Client.
- */
+  /**
+   * Client.
+   */
 
-var client = net.createConnection(8888);
+  var client = net.createConnection(port);
 
-/**
- * Monkey-patch .once()/
- *
- * @param {String} event
- * @param {String} fn
- */
+  /**
+   * Monkey-patch .once()/
+   *
+   * @param {String} event
+   * @param {String} fn
+   */
 
-client.once = function(event, fn){
-  client.on(event, function callback(chunk){
-    client.removeListener(event, callback);
-    fn(chunk);
+  client.once = function(event, fn){
+    client.on(event, function callback(chunk){
+      client.removeListener(event, callback);
+      fn(chunk);
+    });
+  };
+
+  /**
+   * Timeout support.
+   */
+
+  client.setEncoding('utf8');
+  client.setTimeout(1000);
+  client.on('timeout', function(){
+    console.log('timed out');
+    process.exit(1);
   });
-};
 
-/**
- * Timeout support.
- */
+  /**
+   * Test cases.
+   */
 
-client.setEncoding('utf8');
-client.setTimeout(1000);
-client.on('timeout', function(){
-  console.log('timed out');
-  process.exit(1);
-});
+  var files = fs.readdirSync(__dirname + '/cases').filter(function(file){
+    return !~file.indexOf('.out');
+  });
 
-/**
- * Test cases.
- */
+  /**
+   * Pending tests.
+   */
 
-var files = fs.readdirSync(__dirname + '/cases').filter(function(file){
-  return !~file.indexOf('.out');
-});
+  var pending = files.length;
 
-/**
- * Pending tests.
- */
+  /**
+   * Normalize `str`.
+   *
+   * @return {String}
+   */
 
-var pending = files.length;
+  function normalize(str) {
+    return str
+      .replace(/^\-+\n/gm, '')
+      .replace(/\n/g, '\r\n');
+  }
 
-/**
- * Normalize `str`.
- *
- * @return {String}
- */
+  /**
+   * Expose hidden chars in `str`.
+   *
+   * @param {String} str
+   * @return {String}
+   */
 
-function normalize(str) {
-  return str
-    .replace(/^\-+\n/gm, '')
-    .replace(/\n/g, '\r\n');
-}
+  function expose(str) {
+    return str
+      .replace(/\r\n/g, '\\r\\n\n')
+      .replace(/^/gm, '  ');
+  }
 
-/**
- * Expose hidden chars in `str`.
- *
- * @param {String} str
- * @return {String}
- */
+  /**
+   * Run tests.
+   */
 
-function expose(str) {
-  return str
-    .replace(/\r\n/g, '\\r\\n\n')
-    .replace(/^/gm, '  ');
-}
+  client.on('connect', function(){
+    (function next(){
+      file = files.shift();
+      process.stdout.write('\033[90m...\033[0m \033[33m' + file + '\033[0m ');
 
-/**
- * Run tests.
- */
+      // Read test case
+      var input
+        , expected
+        , path = __dirname + '/cases/' + file;
 
-client.on('connect', function(){
-  (function next(){
-    file = files.shift();
-    process.stdout.write('\033[90m...\033[0m \033[33m' + file + '\033[0m ');
-
-    // Read test case
-    var input
-      , expected
-      , path = __dirname + '/cases/' + file;
-
-    // input
-    fs.readFile(path, 'utf8', function(err, str){
-      input = normalize(str);
-      input && expected && test();
-    });
-    
-    // output
-    fs.readFile(path + '.out', 'utf8', function(err, str){
-      expected = '+OK\r\n' + normalize(str);
-      input && expected && test();
-    });
-
-    function test(){
-      var start = new Date;
-      client.write('*1\r\n$7\r\nFLUSHDB\r\n');
-      client.write(input);
-      client.once('data', function(reply){
-        // TODO: fram to prevent race-condition
-
-        // All good
-        if (expected == reply) {
-          console.log('\033[32m%dms\033[0m', new Date - start);
-          --pending || process.exit(0);
-        // Not so good  
-        } else {
-          console.log('\n');
-          console.log('  \033[31m%s:\033[0m ', 'expected');
-          console.log(expose(expected));
-          console.log('  \033[31m%s:\033[0m ', 'got');
-          console.log(expose(reply));
-          process.exit(1);
-        }
-
-        // TATFT
-        next();
+      // input
+      fs.readFile(path, 'utf8', function(err, str){
+        input = normalize(str);
+        input && expected && test();
       });
-    }
-  })();
+
+      // output
+      fs.readFile(path + '.out', 'utf8', function(err, str){
+        expected = '+OK\r\n' + normalize(str);
+        input && expected && test();
+      });
+
+      function test(){
+        var start = new Date;
+        client.write('*1\r\n$7\r\nFLUSHDB\r\n');
+        client.write(input);
+        client.once('data', function(reply){
+          // TODO: fram to prevent race-condition
+
+          // All good
+          if (expected == reply) {
+            console.log('\033[32m%dms\033[0m', new Date - start);
+            --pending || process.exit(0);
+          // Not so good  
+          } else {
+            console.log('\n');
+            console.log('  \033[31m%s:\033[0m ', 'expected');
+            console.log(expose(expected));
+            console.log('  \033[31m%s:\033[0m ', 'got');
+            console.log(expose(reply));
+            process.exit(1);
+          }
+
+          // TATFT
+          next();
+        });
+      }
+    })();
+  });
 });
+server.listen(port);
