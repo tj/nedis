d2b7aa9cb12eee9bf7ddf7b13f9db3dab8c276ce
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2011-03-13 14:38:23 -0400

    get tests working reliably
    now works on ubuntu. closes #1

--- test/test.js
+++ test/test.js
@@ -78,12 +78,14 @@ server.on('listening', function(){
 
   client.on('connect', function(){
     (function next(){
-      file = files.shift();
+      if (!(file = files.shift())) process.exit(0);
+
       process.stdout.write('\033[90m  ' + file + '\033[0m ');
 
       // Read test case
       var input
         , expected
+        , start
         , path = __dirname + '/cases/' + file;
 
       // input
@@ -99,30 +101,33 @@ server.on('listening', function(){
       });
 
       function test(){
-        var start = new Date;
+        expected = expected.trimRight().split(/\r\n/);
+        client.on('data', compare);
+        start = new Date;
         client.write('*1\r\n$7\r\nFLUSHDB\r\n');
         client.write(input);
-        client.once('data', function(reply){
-          // TODO: frame to prevent race-condition
+      }
 
-          // All good
-          if (expected == reply) {
-            console.error('\033[32m%dms\033[0m', new Date - start);
-            --pending || process.exit(0);
-          // Not so good  
+      function compare(reply){
+        reply.trimRight().split(/\r\n/).forEach(function(line){
+          if (line == expected[0]) {
+            expected.shift();
           } else {
-            console.error('\n');
             console.error('  \033[31m%s:\033[0m ', 'expected');
-            console.error(expose(expected));
+            console.error(expose(expected[0]));
             console.error('  \033[31m%s:\033[0m ', 'got');
-            console.error(expose(reply));
+            console.error(expose(line));
             process.exit(1);
           }
+        })
 
-          // TATFT
+        if (!expected.length) {
+          console.error('\033[32m%dms\033[0m', new Date - start);
+          client.removeListener('data', compare);
           next();
-        });
+        }
       }
+
     })();
   });
 });
